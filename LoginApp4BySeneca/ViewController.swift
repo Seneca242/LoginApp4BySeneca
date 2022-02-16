1b14f9c3952c36efaad05c36b886bc6f5979bc10
Seneca <flabbergasted242@gmail.com>
2022-02-16 11:12:11 +0300

    Initial Commit

---
+++ LoginApp4BySeneca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp4BySeneca
+//
+//  Created by Дмитрий Дмитрий on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
